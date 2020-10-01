104889c9fcb91dc8c1379b1a80a31016543742c5
Areiahna <areiahnacooks@yahoo.com>
2020-09-30 23:44:50 -0400

    Layout for bootstrap

--- shoeapp/management/commands/tables.py
+++ shoeapp/management/commands/tables.py
@@ -1,2 +1,13 @@
-class Command(BaseCommand):
-    pass
+from django.core.management.base import BaseCommand, CommandError
+from shoeapp.models import Shoetype, ShoeColor
+# ?? Import bootstrap? - just installed it never used it before
+
+
+class bootstrap_data(BaseCommand):
+
+   # def "?? Function that connects bootstrap to shoeapp"
+    def shoe_color(self, parser):
+        pass
+
+       # "?? Setup like views in other applications? "
+       # the command should populate the model tables with their choices.
